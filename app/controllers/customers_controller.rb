09d47d4081a27e9ba75953f8f2999508c0379e4e
Unknown <spartakus1688@gmail.com>
2019-09-03 11:31:50 +0300

    Add create method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,5 +3,14 @@ class CustomersController < ApplicationController
   end
 
   def create
+    @customer = Customer.new(customer_params)
+
+    @customer.save
+    redirect_to @customer
   end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:name, :phone)
+    end
 end
