9e70a78ef2c673e406ad184c909e05765e12a406
Unknown <spartakus1688@gmail.com>
2019-09-03 11:59:46 +0300

    Add method show

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,13 @@
 class CustomersController < ApplicationController
+
   def index
     @customers = Customer.all
   end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
   def new
   end
 
