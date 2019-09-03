7f6c88babb7fc64aa74908e5190a24f11771f96d
Unknown <spartakus1688@gmail.com>
2019-09-03 12:26:54 +0300

    Add data seed for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+6.times do |i|
+  Customer.create(name: "Customer name ##{i}", phone: "#{i}", description: "Description at Customer №##{i}")
+end
