f7aa7effab2d379327be2939f105e7763093c80c
Unknown <spartakus1688@gmail.com>
2019-09-03 09:51:09 +0300

    New root page 'customer_service'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'customer_service/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'customer_service#index'
 end
