3332c9a5e988941b8ec36930fd3cee19549538e9
Unknown <spartakus1688@gmail.com>
2019-09-03 11:15:15 +0300

    Generate controller customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'customer_service/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :customers
+  
   root 'customer_service#index'
 end
